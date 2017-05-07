9d4b26aa9065479d2ae3eaa7918e267545a1f5f2
NAKAMURA Yoshitaka <arumakanoy@gmail.com>
2017-05-08 01:04:54 +0900

    bump version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='konnyaku',
-    version='0.0.1',
+    version='0.0.2',
     description='websites update checker',
     author='NAKAMURA Yoshitaka',
     author_email='arumakanoy@gmail.com',
